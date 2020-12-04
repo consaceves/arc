193d3ff709e8025f60ce1fafb028b878efbdd737
Ruohuang Zheng <zhengruohuang@gmail.com>
2020-12-04 16:44:44 -0500

    fixed push and pull, and some cleanup

--- arc/src/main.rs
+++ arc/src/main.rs
@@ -4,9 +4,179 @@ mod mach;
 mod revid;
 mod repo;
 mod rev;
+mod merge;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    println!("Args: {:?}", args);
-    cmd::command();
+    let matches = App::new("arc")
+        .version("1.0")
+        .about("DVCS System in Rust")
+        .author("Aceves, Zimmerman, Zheng")
+        .subcommand(
+            App::new("init")
+                .about("Initialize repository")
+                .arg(Arg::new("directory").about("Directory path").required(false))        
+            )
+        .subcommand(
+            App::new("print")
+                .about("Prints a repository")
+                .arg(Arg::new("path").about("The repository user wants to print").required(false)),
+        )
+        .subcommand(
+            App::new("clone")
+                .about("Clone a repository")
+                .arg(Arg::new("src").about("The source repository").required(true))
+                .arg(Arg::new("dst").about("The destination repository").required(false))
+        )
+        .subcommand(
+            App::new("add")
+                .about("Add specific files that user wants to track")
+                .arg(Arg::new("path").about("Path of the added file").required(true))
+        )
+        .subcommand(
+            App::new("remove")
+                .about("Remove specific files from tracking list")
+                .arg(Arg::new("path").about("Path of the removed file").required(true))
+        )
+        .subcommand(
+            App::new("heads")
+                .about("Show the current heads")
+        )
+        .subcommand(
+            App::new("diff")
+                .about("Check the changes between revisions")
+        )
+        .subcommand(
+            App::new("cat")
+                .about("Inspect a file of a given revision")
+                .arg(Arg::new("path").about("File path of inspected file").required(true))
+        )
+        .subcommand(
+            App::new("checkout")
+                .about("Check out a specific revision")
+                .arg(Arg::new("rev").about("Revision to checkout to").required(true))
+        )
+        .subcommand(
+            App::new("commit")
+                .about("Commit changes")
+        )
+        .subcommand(
+            App::new("merge")
+                .about("Merge two revisions")
+                .arg(Arg::new("rev1").about("First revision to merge").required(true))
+                .arg(Arg::new("rev2").about("Second revision to merge").required(true))
+        )
+        .subcommand(
+            App::new("push")
+                .about("Push changes")
+        )
+        .subcommand(
+            App::new("pull")
+                .about("Pull changes")
+        )
+        .get_matches();
+
+        match matches.subcommand() {
+            Some(("init", init_matches)) => {
+                let mut args = Vec::new();
+                let mut directory = String::new();
+                if init_matches.is_present("directory") {
+                    directory = init_matches.value_of("directory").unwrap().to_string();
+                } else {
+                    directory = mach::get_cwd();
+                }
+                args.push(&directory);
+                cmd::command("init".to_string(), args)
+            }
+            Some(("print", print_matches)) => {
+                let mut args = Vec::new();
+                let mut path = String::new();
+                if print_matches.is_present("path") {
+                    path = print_matches.value_of("path").unwrap().to_string();
+                } else {
+                    path = mach::get_cwd();
+                }
+                args.push(&path);
+                cmd::command("print".to_string(), args)
+            }
+            Some(("clone", clone_matches)) => {
+                let mut args = Vec::new();
+                let mut src = String::new();
+                let mut dst = String::new();
+                if clone_matches.is_present("src") {
+                    src = clone_matches.value_of("src").unwrap().to_string();
+                }
+                args.push(&src);
+                if clone_matches.is_present("dst") {
+                    dst = clone_matches.value_of("dst").unwrap().to_string();
+                } else {
+                    dst = mach::get_cwd();
+                }
+                args.push(&dst);
+                cmd::command("clone".to_string(), args)
+            }
+            Some(("add", add_matches)) => {
+                let mut args = Vec::new();
+                let mut path = String::new();
+                if add_matches.is_present("path") {
+                    path = add_matches.value_of("path").unwrap().to_string();
+                }
+                args.push(&path);
+                cmd::command("add".to_string(), args);
+            }
+            Some(("remove", remove_matches)) => {
+                let mut args = Vec::new();
+                let mut path = String::new();
+                if remove_matches.is_present("path") {
+                    path = remove_matches.value_of("path").unwrap().to_string();
+                }
+                args.push(&path);
+                cmd::command("remove".to_string(), args);
+            }
+            Some(("heads", heads_matches)) => {
+                println!("arc heads was used");
+            }
+            Some(("diff", diff_matches)) => {
+                println!("arc diff was used");
+            }
+            Some(("cat", cat_matches)) => {
+                println!("arc cat was used");
+            }
+            Some(("checkout", checkout_matches)) => {
+                let mut args = Vec::new();
+                let mut rev = String::new();
+                if checkout_matches.is_present("rev") {
+                    rev = checkout_matches.value_of("rev").unwrap().to_string();
+                }
+                args.push(&rev);
+                cmd::command("checkout".to_string(), args);
+            }
+            Some(("commit", commit_matches)) => {
+                let args = Vec::new();
+                cmd::command("commit".to_string(), args)
+            }
+            Some(("merge", merge_matches)) => {
+                let mut args = Vec::new();
+                let mut rev1 = String::new();
+                let mut rev2 = String::new();
+                if merge_matches.is_present("rev1") {
+                    rev1 = merge_matches.value_of("rev1").unwrap().to_string();
+                }
+                if merge_matches.is_present("rev2") {
+                    rev2 = merge_matches.value_of("rev2").unwrap().to_string();
+                }
+                args.push(&rev1);
+                args.push(&rev2);
+                cmd::command("merge".to_string(), args);
+            }
+            Some(("push", push_matches)) => {
+                let args = Vec::new();
+                cmd::command("push".to_string(), args)
+            }
+            Some(("pull", pull_matches)) => {
+                let args = Vec::new();
+                cmd::command("pull".to_string(), args)
+            }
+            None => println!("No subcommand was used"),
+            _ => unreachable!(), 
+        }
 }
--- arc/src/rev.rs
+++ arc/src/rev.rs
@@ -1,28 +1,156 @@
 use serde::{Serialize, Deserialize};
+use std::fmt;
 use crate::mach;
+use crate::revid;
+use crate::revid::RevID;
+use crate::repo;
+use crate::merge;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Rev {
-    pub rev_id: String,
-    
-    pub parent_trunk: String,
-    pub parent_other: String,
-    
-    pub files: Vec<String>,
+struct RevInfo {
+    rev_id: RevID,
+    parent_trunk: RevID,
+    parent_other: RevID,
+    files: Vec<String>,
 }
 
-impl Rev {
-    pub fn save(&self, rev_path: &String) {
-        let serialized = serde_json::to_string(self).unwrap();
-        mach::write_string(&rev_path, &String::from("rev.json"), &serialized);
+pub struct Rev {
+    pub root_path: String,
+    pub arc_path: String,
+    pub rev_path: String,
+    rev: RevInfo,
+}
+
+impl fmt::Display for RevInfo {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Revision ID: {}\n", self.rev_id)?;
+        write!(f, "Parent ID (trunk): {}", self.parent_trunk)?;
+        write!(f, "Parent ID (other): {}", self.parent_other)?;
+        write!(f, "All Files:\n")?;
+        for l in &self.files {
+            write!(f, "  {}\n", l.to_string())?;
+        }
+        Ok(())
     }
 }
 
-pub fn open_rev(rev_path: &String) -> Rev {
-    // TODO: check if rev_path exists
+impl fmt::Display for Rev {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Root Path @ {}\n", self.root_path)?;
+        write!(f, ".arc Path @ {}\n", self.arc_path)?;
+        write!(f, "Revision Path @ {}\n", self.rev_path)?;
+        write!(f, "{}", self.rev)?;
+        Ok(())
+    }
+}
+
+impl Rev {
+    pub fn save(&self) {
+        let serialized = serde_json::to_string(&self.rev).unwrap();
+        mach::write_string(&self.rev_path, &String::from("rev.json"), &serialized);
+    }
+    
+    pub fn get_id(&self) -> &RevID {
+        &self.rev.rev_id
+    }
+    
+    pub fn get_id_str(&self) -> String {
+        self.rev.rev_id.to_string()
+    }
+    
+    pub fn get_files(&self) -> &Vec<String> {
+        &self.rev.files
+    }
+    
+    pub fn get_parent_trunk_id(&self) -> &RevID {
+        &self.rev.parent_trunk
+    }
+    
+    pub fn get_parent_other_id(&self) -> &RevID {
+        &self.rev.parent_other
+    }
+    
+    pub fn commit(&mut self, tracked_files: &Vec<String>) -> Vec<String> {
+        let mut missing_files = Vec::new();
+        for f_rel_path in tracked_files {
+            if mach::check_path(&mach::join_paths(&self.root_path, &f_rel_path)) {
+                mach::copy_file(&self.rev_path, &self.root_path, f_rel_path);
+                self.rev.files.push(f_rel_path.clone());
+            } else {
+                missing_files.push(f_rel_path.clone());
+            }
+        }
+        missing_files
+    }
+    
+    pub fn checkout(&self) {
+        for f_rel_path in &self.rev.files {
+            assert!(mach::check_path(&mach::join_paths(&self.rev_path, &f_rel_path)), "File missing in a revision!");
+            mach::copy_file(&self.root_path, &self.rev_path, f_rel_path);
+        }
+    }
+    
+    pub fn copy_to(&self, dst_path: &String) {
+        mach::create_dir_all(dst_path);
+        
+        for f_rel_path in &self.rev.files {
+            assert!(mach::check_path(&mach::join_paths(&self.root_path, &f_rel_path)), "File missing in a revision!");
+            mach::copy_file(&dst_path, &self.rev_path, f_rel_path);
+        }
+        mach::copy_file(&dst_path, &self.rev_path, &"rev.json".to_string());
+    }
+    
+    pub fn merge(&mut self, ancestor_rev: &Rev, trunk_rev: &Rev, other_rev: &Rev) {
+        let ancestor_files = ancestor_rev.get_files();
+        let trunk_files = trunk_rev.get_files();
+        let other_files = other_rev.get_files();
+        
+        let files = merge::find_all_files(ancestor_files, trunk_files, other_files);
+        for f in &files {
+            let ancestor_content = if ancestor_files.contains(&f) { Some(mach::read_line(&ancestor_rev.rev_path, &f)) } else { None };
+            let trunk_content = if trunk_files.contains(&f) { Some(mach::read_line(&trunk_rev.rev_path, &f)) } else { None };
+            let other_content = if other_files.contains(&f) { Some(mach::read_line(&other_rev.rev_path, &f)) } else { None };
+            
+            let m = merge::merge3(ancestor_content, trunk_content, other_content);
+            if m.is_some() {
+                mach::write_string(&self.rev_path, &f, &m.unwrap());
+                self.rev.files.push(f.clone());
+            }
+        }
+    }
+}
+
+pub fn new(repo: &repo::Repo, trunk_id: &RevID, other_id: &RevID) -> Rev {
+    let rev = RevInfo {
+        rev_id: revid::new(),
+        parent_trunk: trunk_id.clone(),
+        parent_other: other_id.clone(),
+        files: Vec::new(),
+    };
+    
+    let rev_path = mach::join_paths(&repo.arc_path, &rev.rev_id.to_string());
+    mach::create_dir_all(&rev_path);
+    
+    Rev {
+        root_path: repo.root_path.clone(),
+        arc_path: repo.arc_path.clone(),
+        rev_path: rev_path.clone(),
+        rev: rev
+    }
+}
+
+pub fn open(repo: &repo::Repo, rev_id: &RevID) -> Rev {
+    let rev_path = mach::join_paths(&repo.arc_path, &rev_id.to_string());
+    assert!(mach::check_path(&rev_path), "Revision dir doesn't exist!");
     
     let json = mach::read_line(&rev_path, &String::from("rev.json"));
-    let r: Rev = serde_json::from_str(&json).unwrap();
-    r
+    let r: RevInfo = serde_json::from_str(&json).expect("Unable to open revision, bad rev file!");
+    
+    Rev {
+        root_path: repo.root_path.clone(),
+        arc_path: repo.arc_path.clone(),
+        rev_path: rev_path.clone(),
+        rev: r
+    }
 }
 
